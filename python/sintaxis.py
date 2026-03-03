91589bee75609b9430dcf6795ab8b3ddc2d49690
stivenearias <bsea096@gmail.com>
2026-03-03 16:31:19 -0500

    add: clase de sintaxis hasta el tema de condicionales

---
+++ python/sintaxis.py
@@ -0,0 +1,41 @@
+# SINTAXIS
+if 5 > 2:
+  print("5 Es mayor que 2")
+
+# Comentario de una linea
+print("Hola Stiven") # Saludar al desarrollador
+
+"""
+Comentario
+De
+Multiples
+Lineas
+"""
+
+# Variables y tipo de dato
+nombreVariable = "" #string
+nombreVariable = '' #string
+nombreVariableInt = 123 #int
+nombreVariableFloat = 123.123 #float
+nombreVariableBoolean = True #bool
+
+# Condicionales
+"""
+Crear un programa que muestre si el usuario es 
+mayor de edad o no.
+"""
+
+# Pedimos al usuario que ingrese su edad para ser validada
+edad = 15
+
+# Validar con condicional si la edad es mayor o igual a 18
+if edad >= 18:
+  print("El usuario es mayor de edad.")
+else:
+  print("El usuario es menor de edad.")
+
+
+
+
+
+
